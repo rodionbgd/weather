a4a567072479794de9ea02a2f73b69174ac3addb
rodionbgd <rodionbgd@mail.ru>
2022-02-13 12:26:49 +0300

    fix: Убран лишний импорт в webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
